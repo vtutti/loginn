b42e838d6d10d179a9aeae9a562878c6d13bab8a
vtutti <76520252+vtutti@users.noreply.github.com>
2021-02-10 16:51:07 +0530

    Update index.php

--- login/index.php
+++ login/index.php
@@ -14,10 +14,10 @@ include_once("db_connect.php");
 		<br>
 		<br>
 		<div class="collapse navbar-collapse" id="navbar1">
-			<table>
+			<table class="center">
 				<tr>
 					<td>
-			<ul class="nav navbar-nav navbar-left">
+			<ul class="nav navbar-nav navbar-left" style="font-size:30px">
 				<?php if (isset($_SESSION['user_id'])) { ?>
 				<li><p class="navbar-text"><strong>Welcome!</strong> You're signed in as <strong><?php echo $_SESSION['user_name']; ?></strong></p></li>
 				<li><a href="logout.php">Log Out</a></li>
@@ -26,7 +26,7 @@ include_once("db_connect.php");
 				<li><a href="register.php">Sign Up</a></li>
 				<?php } ?>
 				        </td>
-					<td <td colspan = 4><img src="giphy.gif" alt="" ></img>
+					<td <td rowspan = 4 colspan = 4><img src="giphy.gif" alt="" ></img>
 						
 				        </td>
 			</ul>
