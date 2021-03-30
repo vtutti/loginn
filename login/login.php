fdc3f7981474da53a873d53a0a0f19775e03da60
vtutti <76520252+vtutti@users.noreply.github.com>
2021-03-30 20:13:37 +0530

    Update login.php

--- login/login.php
+++ login/login.php
@@ -24,7 +24,7 @@ if (isset($_POST['login'])) {
 <?php include('../container.php');?>
 
 <div class="container">
-	<h2>HEARTCHECK LOGIN </h2>		
+	<h2> </h2>		
 	<div class="row">
 		<div class="col-md-4 col-md-offset-4 well">
 			<form role="form" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" name="loginform">
